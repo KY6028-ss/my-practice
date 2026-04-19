f3f6eae40f1bb3fe20266355e100ea45907021d5
KY6028-ss <onigiri.2006zzz@gmail.com>
2026-04-19 15:13:53 +0900

    print関数

--- basic.py
+++ basic.py
@@ -27,3 +27,35 @@ print("-----------------------------------------")
 #sepは区切り文字、endは出力後の文字
 print('Hi','Mike',end='ーー')
 print('Hi','Mike',sep=',',end='\n')
+print("-----------------------------------------")
+#値
+print(1+1,1-1,1*1,1/1,1%1,1**1,1//1,sep='\n')
+pie = 3.14159
+print("丸めた円周率は" + str(round(pie, 2)))
+#mathモジュールで計算できる
+import math
+result = math.sqrt(25)
+print("25の平方根は" + str(result))
+
+y = math.log2(2)
+print("2の対数は" + str(y))
+
+#print(help(math))
+print("-----------------------------------------")
+#文字列の出力
+print("Hello World")
+print('Hello World')
+print("I don't know")
+print('I don\'t know')
+print("say \"I don't know\"")
+print("hello.\nHow are you?")
+print(r"C:\name\name")#raw文字列
+print("-----------------------------------------")
+#文字列の連結
+print("""\
+line1
+line2
+line3\
+""")
+print("-----------------------------------------")
+print("")
